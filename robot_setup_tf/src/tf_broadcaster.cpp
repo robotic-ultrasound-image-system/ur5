4595a62be774f82cc57040064928ebcf81c42e2b
VCBE123 <vcbe123@gmail.com>
2017-09-03 17:39:14 +0800

    add tf

---
+++ robot_setup_tf/src/tf_broadcaster.cpp
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc ,char** argv)
+ {
+	ros::init(argc,argv,"robot_tf_publisher");
+	ros::NodeHandle n;
+	
+	ros::Rate r(50);
+	tf::TransformBroadcaster broadcaster;
+
+	while(n.ok()){
+	broadcaster.sendTransform(
+	  tf::StampedTransform(
+		tf::Transform(tf::Quaternion(0.1451,-2.3788,1.8576 ),tf.Vector3(0.0431,0.3950,1.2239))
+		ros::Time::now(),"base_link","kinect2_link"));
+
+	}
+
+
+
+ }
