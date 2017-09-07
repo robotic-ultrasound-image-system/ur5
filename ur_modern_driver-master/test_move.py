c31ee30c50417e6b88b40acaa1dfeb38992435ba
VCBE123 <vcbe123@gmail.com>
2017-09-07 16:36:31 +0800

    2017.9.7

--- ur_modern_driver-master/test_move.py
+++ ur_modern_driver-master/test_move.py
@@ -10,57 +10,13 @@ from math import pi
 
 JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
                'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+
+
+
 Q1 = [2.2,0,-1.57,0,0,0]
 Q2 = [1.5,0,-1.57,0,0,0]
-Q3 = [1.5,-0.2,-1.57,0,0,0]
     
 client = None
-
-def move1():
-    global joints_pos
-    g = FollowJointTrajectoryGoal()
-    g.trajectory = JointTrajectory()
-    g.trajectory.joint_names = JOINT_NAMES
-    try:
-        joint_states = rospy.wait_for_message("joint_states", JointState)
-        joints_pos = joint_states.position
-        g.trajectory.points = [
-            JointTrajectoryPoint(positions=joints_pos, velocities=[0]*6, time_from_start=rospy.Duration(0.0)),
-            JointTrajectoryPoint(positions=Q1, velocities=[0]*6, time_from_start=rospy.Duration(2.0)),
-            JointTrajectoryPoint(positions=Q2, velocities=[0]*6, time_from_start=rospy.Duration(3.0)),
-            JointTrajectoryPoint(positions=Q3, velocities=[0]*6, time_from_start=rospy.Duration(4.0))]
-        client.send_goal(g)
-        client.wait_for_result()
-    except KeyboardInterrupt:
-        client.cancel_goal()
-        raise
-    except:
-        raise
-
-def move_disordered():
-    order = [4, 2, 3, 1, 5, 0]
-    g = FollowJointTrajectoryGoal()
-    g.trajectory = JointTrajectory()
-    g.trajectory.joint_names = [JOINT_NAMES[i] for i in order]
-    q1 = [Q1[i] for i in order]
-    q2 = [Q2[i] for i in order]
-    q3 = [Q3[i] for i in order]
-    try:
-        joint_states = rospy.wait_for_message("joint_states", JointState)
-        joints_pos = joint_states.position
-        g.trajectory.points = [
-            JointTrajectoryPoint(positions=joints_pos, velocities=[0]*6, time_from_start=rospy.Duration(0.0)),
-            JointTrajectoryPoint(positions=q1, velocities=[0]*6, time_from_start=rospy.Duration(2.0)),
-            JointTrajectoryPoint(positions=q2, velocities=[0]*6, time_from_start=rospy.Duration(3.0)),
-            JointTrajectoryPoint(positions=q3, velocities=[0]*6, time_from_start=rospy.Duration(4.0))]
-        client.send_goal(g)
-        client.wait_for_result()
-    except KeyboardInterrupt:
-        client.cancel_goal()
-        raise
-    except:
-        raise
-    
 def move_repeated():
     g = FollowJointTrajectoryGoal()
     g.trajectory = JointTrajectory()
@@ -77,9 +33,6 @@ def move_repeated():
             g.trajectory.points.append(
                 JointTrajectoryPoint(positions=Q2, velocities=[0]*6, time_from_start=rospy.Duration(d)))
             d += 2
-            g.trajectory.points.append(
-                JointTrajectoryPoint(positions=Q3, velocities=[0]*6, time_from_start=rospy.Duration(d)))
-            d += 3
         client.send_goal(g)
         client.wait_for_result()
     except KeyboardInterrupt:
@@ -88,37 +41,7 @@ def move_repeated():
     except:
         raise
 
-def move_interrupt():
-    g = FollowJointTrajectoryGoal()
-    g.trajectory = JointTrajectory()
-    g.trajectory.joint_names = JOINT_NAMES
-    try:
-        joint_states = rospy.wait_for_message("joint_states", JointState)
-        joints_pos = joint_states.position
-        g.trajectory.points = [
-            JointTrajectoryPoint(positions=joints_pos, velocities=[0]*6, time_from_start=rospy.Duration(0.0)),
-            JointTrajectoryPoint(positions=Q1, velocities=[0]*6, time_from_start=rospy.Duration(2.0)),
-            JointTrajectoryPoint(positions=Q2, velocities=[0]*6, time_from_start=rospy.Duration(3.0)),
-            JointTrajectoryPoint(positions=Q3, velocities=[0]*6, time_from_start=rospy.Duration(4.0))]
-    
-        client.send_goal(g)
-        time.sleep(3.0)
-        print "Interrupting"
-        joint_states = rospy.wait_for_message("joint_states", JointState)
-        joints_pos = joint_states.position
-        g.trajectory.points = [
-            JointTrajectoryPoint(positions=joints_pos, velocities=[0]*6, time_from_start=rospy.Duration(0.0)),
-            JointTrajectoryPoint(positions=Q1, velocities=[0]*6, time_from_start=rospy.Duration(2.0)),
-            JointTrajectoryPoint(positions=Q2, velocities=[0]*6, time_from_start=rospy.Duration(3.0)),
-            JointTrajectoryPoint(positions=Q3, velocities=[0]*6, time_from_start=rospy.Duration(4.0))]
-        client.send_goal(g)
-        client.wait_for_result()
-    except KeyboardInterrupt:
-        client.cancel_goal()
-        raise
-    except:
-        raise
-   
+
 def main():
     global client
     try:
@@ -137,7 +60,6 @@ def main():
         print "This program makes the robot move between the following three poses:"
         print str([Q1[i]*180./pi for i in xrange(0,6)])
         print str([Q2[i]*180./pi for i in xrange(0,6)])
-        print str([Q3[i]*180./pi for i in xrange(0,6)])
         print "Please make sure that your robot can move freely between these poses before proceeding!"
         inp = raw_input("Continue? y/n: ")[0]
         if (inp == 'y'):
