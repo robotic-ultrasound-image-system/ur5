01e069750b5ef1e47bb53dc35f3b2827e7bd1c9e
VCBE123 <vcbe123@gmail.com>
2017-09-19 11:05:34 +0800

    failed test

--- ur5-master/universal_robot-kinetic-devel/ur_modern_driver-master/testik.py
+++ ur5-master/universal_robot-kinetic-devel/ur_modern_driver-master/testik.py
@@ -6,35 +6,61 @@ import roslib; roslib.load_manifest('ur_modern_driver')
 from control_msgs.msg import *
 from trajectory_msgs.msg import *
 from sensor_msgs.msg import JointState
-
-
+from tf.transformations import euler_from_quaternion ,quaternion_from_euler
+from geometry_msgs.msg import  PoseStamped,Pose
+ 
 client = None
 def move_joint():
     try:
       moveit_commander.roscpp_initialize(sys.argv)
+      
       arm=moveit_commander.MoveGroupCommander('manipulator')
+      
       end_effector_link=arm.get_end_effector_link()
-      rospy.loginfo("the end effector link is:"+str(end_effector_link))
+      reference_frame='base_link'
+      arm.set_pose_reference_frame(reference_frame)
+
+      arm.allow_replanning(True)
+      arm.set_goal_position_tolerance(0.01)
+      arm.set_goal_orientation_tolerance(0.1)
+    
       arm.set_named_target("home")
+      arm.go()
+      rospy.sleep(2)
+      
+      
+   
+      
+      target_pose=PoseStamped()
+      target_pose.header.stamp=rospy.Time.now()
+      target_pose.pose.position.x=-0.40
+      target_pose.pose.position.y=-0.40
+      target_pose.pose.position.z=0.20
+      target_pose.pose.orientation.x=1.5
+      target_pose.pose.orientation.y=-0.3
+      target_pose.pose.orientation.z=0.1
+      target_pose.pose.orientation.w=1.0
+
+  
+      
+      arm.set_pose_target(target_pose,end_effector_link)
+      arm.set_start_state_to_current_state()
+      
       traj=arm.plan()
       arm.execute(traj)
-      rospy.sleep(5)
+      rospy.sleep(2)
+     
 
-      joint_position=[0,0,0,0,-0.7,-0.7]
-      arm.set_joint_value_target(joint_position)
-      arm.go()
-   
-      rospy.sleep(5)
 
-      arm.remember_joint_values('saved',joint_position)
-      arm.set_named_target("up")
+      saved_target_pose=arm.get_current_pose(end_effector_link)
+      arm.set_named_target("home")
       arm.go()
-      rospy.sleep(5)
-      arm.set_named_target('saved')
+      rospy.sleep(1)
+      
+      arm.set_pose_target(saved_target_pose,end_effector_link)
       arm.go()
-      rospy.sleep(5)
-      arm.set_named_target("up")
-      arm.go()
+      rospy.sleep(1)
+     
       moveit_commander.roscpp_shutdown()
     except KeyboardInterrupt:
         client.cancel_goal()
